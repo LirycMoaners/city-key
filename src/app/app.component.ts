584b33166b2fa8085fd3c7b8c3b8927f87c448f6
Cyril Lefebvre <cyril.lefebvre.27@gmail.com>
2020-12-09 17:18:20 +0100

    feat(game) Add GameModule & GameComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { map, catchError } from 'rxjs/operators';
-import { GoogleMap } from '@angular/google-maps';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -11,17 +7,7 @@ import { GoogleMap } from '@angular/google-maps';
 })
 export class AppComponent {
   title = 'city-key';
-  apiLoaded: Observable<boolean>;
-  @ViewChild('test') googleMap: ElementRef;z
 
-  constructor(httpClient: HttpClient) {
-    this.apiLoaded = httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyCRnkz903chyNcwW5W3A0_nWHE359oLdlw', 'callback')
-        .pipe(
-          map(() => {
-            new google.maps.Map(this.googleMap.nativeElement, {mapId: 'a6e118a22ba119fa'});
-            return true;
-          }),
-          catchError(() => of(false))
-        );
+  constructor() {
   }
 }
---
+++ src/app/game/game.component.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.scss']
+})
+export class GameComponent implements OnInit {
+  apiLoaded: Observable<boolean>;
+  @ViewChild('test') googleMap: ElementRef;
+
+  constructor(private httpClient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.apiLoaded = this.httpClient.jsonp('https://maps.googleapis.com/maps/api/js?key=AIzaSyCRnkz903chyNcwW5W3A0_nWHE359oLdlw', 'callback')
+        .pipe(
+          map(() => {
+            new google.maps.Map(this.googleMap.nativeElement, {mapId: 'a6e118a22ba119fa'});
+            return true;
+          }),
+          catchError(() => of(false))
+        );
+  }
+
+}
