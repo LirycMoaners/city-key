e4957c683830a86376f1a39ec818621d094aaf6e
FeliciaMarlove <missflorencemary@hotmail.com>
2020-12-14 19:37:00 +0100

    feat: mechanism list component in game module, routing

--- src/app/game/game.module.ts
+++ src/app/game/game.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { GameComponent } from './game.component';
 import { SharedModule } from '../shared/shared.module';
 import { GameRoutingModule } from './game-routing.module';
+import { MechanismListComponent } from './mechanism-list/mechanism-list.component';
 
 
 
 @NgModule({
-  declarations: [GameComponent],
+  declarations: [GameComponent, MechanismListComponent],
   imports: [
     GameRoutingModule,
     CommonModule,
