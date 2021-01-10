13635b3e4fd8ce8bb0bde00d8345e9ae4fe1b020
Cyril Lefebvre <cyril.lefebvre.27@gmail.com>
2021-01-10 23:54:01 +0100

    feat(GameComponent) Check geolocation & game to progress in scenario

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription, } from 'rxjs';
+import { combineLatest, Observable, of, Subscription, } from 'rxjs';
+import { map } from 'rxjs/operators';
 import * as mapStyle from '../../assets/styles/map-style.json';
 import { GameService } from '../core/http-services/game.service';
 import { GoogleMapService } from '../core/http-services/google-map.service';
@@ -12,7 +13,6 @@ import { Step } from '../shared/models/step.model';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit, OnDestroy {
-  public game: Game;
   public isGoogleMapApiLoaded$: Observable<boolean>;
   public center: google.maps.LatLngLiteral = { lat: 48.8581929, lng: 2.3508915 };
   public options: google.maps.MapOptions = {
@@ -32,14 +32,13 @@ export class GameComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
-    this.initGeolocation();
     this.subscriptions.push(
-      this.gameService.getCurrentGame().subscribe(game => {
-        this.game = game;
-        if (!this.game.reachableSteps.length) {
-          this.initGame();
-        }
-      })
+      combineLatest([
+        this.initGeolocation(),
+        this.initGame()
+      ]).pipe(
+        map(([playerPosition, game]: [google.maps.LatLngLiteral, Game]) => this.checkSteps(playerPosition, game))
+      ).subscribe()
     );
   }
 
@@ -52,23 +51,66 @@ export class GameComponent implements OnInit, OnDestroy {
   /**
    * Initialize the center position of the map & the marker position of the player
    */
-  private initGeolocation(): void {
+  private initGeolocation(): Observable<google.maps.LatLngLiteral> {
     if (navigator.geolocation) {
-      navigator.geolocation.watchPosition((position) => {
-        this.center = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-        };
-        this.markerPositions = [{...this.center}];
+      return new Observable<google.maps.LatLngLiteral>(obs => {
+        if (this.googleMapService.lastPosition) {
+          this.center = { ...this.googleMapService.lastPosition };
+          this.markerPositions = [{...this.googleMapService.lastPosition}];
+          obs.next(this.googleMapService.lastPosition);
+        }
+        navigator.geolocation.watchPosition((position) => {
+          this.googleMapService.lastPosition = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          this.center = { ...this.googleMapService.lastPosition };
+          this.markerPositions = [{...this.googleMapService.lastPosition}];
+          obs.next(this.googleMapService.lastPosition);
+        });
       });
     }
+    return of(null);
   }
 
-  private initGame(): void {
-    const firstStep: Step = this.game.scenario.steps.find(step => step.isFirstStep);
-    this.game.items = firstStep.unlockedItems;
-    this.game.mechanisms = firstStep.unlockedMechanisms;
-    this.game.reachableSteps = this.game.scenario.steps.filter(step => firstStep.unlockedStepsId.includes(step.id));
-    this.gameService.updateCurrentGame(this.game);
+  private initGame(): Observable<Game> {
+    return this.gameService.getCurrentGame().pipe(
+      map(game => {
+        if (!game.reachableSteps.length) {
+          const firstStep: Step = game.scenario.steps.find(step => step.isFirstStep);
+          game.reachableSteps.push(firstStep);
+        }
+        return game;
+      })
+    );
+  }
+
+  private checkSteps(playerPosition: google.maps.LatLngLiteral, game: Game): Observable<void> {
+    if (game.reachableSteps.length) {
+      let isUpdateNeeded = false;
+      for (const step of game.reachableSteps) {
+        if (
+          (
+            !step.requiredMechanismsId
+            || !step.requiredMechanismsId.length
+            || step.requiredMechanismsId.every(id => game.completedMechanismsId.includes(id))
+          )
+          && (
+            !step.requiredPosition
+            || (step.requiredPosition.lat === playerPosition.lat && step.requiredPosition.lng === playerPosition.lng)
+          )
+        ) {
+          game.items.push(...step.unlockedItems);
+          game.mechanisms.push(...step.unlockedMechanisms);
+          game.reachableSteps.push(...step.unlockedStepsId.map(id => game.scenario.steps.find(s => s.id === id)));
+          game.reachableSteps.splice(game.reachableSteps.indexOf(step), 1);
+          isUpdateNeeded = true;
+        }
+      }
+      if (isUpdateNeeded) {
+        return this.gameService.updateCurrentGame(game);
+      }
+    }
+    return of();
   }
 }
